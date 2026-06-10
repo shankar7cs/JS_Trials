9e3ec449a20b7f80c07a7fe40b78b3740bb8a8b1
shankar7cs <shankar7cs@gmail.com>
2026-06-10 22:51:19 +0530

    Delete dataTypetwo.js

--- dataTypetwo.js
+++
@@ -1,9 +0,0 @@
-var firstname = "shankar"
-var companyname = "ABC"
-var mobileNumber = 1111122222
-var isAutomation = true 
-var hasPlaywright 
-console.log(firstname, companyname, mobileNumber, isAutomation, hasPlaywright);
-console.log(typeof firstname, typeof companyname, typeof mobileNumber, typeof isAutomation, typeof hasPlaywright);
-
-//assignment was initially provided on let but we went with var
