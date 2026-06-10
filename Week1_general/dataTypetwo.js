e9d54de0ff3b3da385dca71b0e4415ccea97eaf1
shankar7cs <shankar7cs@gmail.com>
2026-06-10 22:47:07 +0530

    Add files via upload

---
+++ Week1_general/dataTypetwo.js
@@ -0,0 +1,9 @@
+var firstname = "shankar"
+var companyname = "ABC"
+var mobileNumber = 1111122222
+var isAutomation = true 
+var hasPlaywright 
+console.log(firstname, companyname, mobileNumber, isAutomation, hasPlaywright);
+console.log(typeof firstname, typeof companyname, typeof mobileNumber, typeof isAutomation, typeof hasPlaywright);
+
+//assignment was initially provided on let but we went with var
